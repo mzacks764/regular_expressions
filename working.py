ec3c57867f29c6c2b2fe26a53eedd5c7e59b2fd1
mzacks764 <mzacks64@outlook.com>
2024-10-08 17:40:03 -0400

    added comments

--- working.py
+++ working.py
@@ -2,9 +2,7 @@ import re
 import sys
 def main():
     full_str = input("Hours: ")
-    times = find_times(full_str)
-    print(times)
-
+    times = find_times(full_str)  # send find_times to extract times
     start_time = convert_military(times[0])
     end_time = convert_military(times[1])
     print(f'{start_time} to {end_time}')
@@ -19,22 +17,29 @@ def find_times(full_str):
         #print(f'first:{first}, second:{second}')
         return first,second
     else:
-        raise ValueError("Use 'to' between times")
+        raise ValueError("Include AM or PM and use 'to' between times")
 
 
 
 def convert_military(time):
+    '''
+    first check to see if time entered is in form 11:00 AM
+    '''
     matches = re.search(r"(\d+):(\d+) ([AP]M)",time)
     if matches:
         #print(f'trying match with colon: {matches.groups()}')
         hours,minutes,meridian = matches.groups()
         #print(f'hours:{hours}  minutes{minutes} meridian {meridian}')
-        if int(minutes) >59:
+        if int(minutes) >59:            # verify that minutes is less than 60
             raise ValueError("Minutes should be 59 or less")
             sys.exit()
-        elif meridian == 'PM':
+        elif meridian == 'PM':          # if time is PM add 12 hours to hours to convert
+                                        # to military time
             hours = int(hours) + 12
-        return f'{hours}:{minutes}'
+        return f'{hours}:{minutes}'     # return time in military time
+    '''
+    next check to see if time entered in form 9 AM
+    '''
     else:
         matches = re.search(r"(\d+) ([AP]M)",time)                  # searches for digit or digits
         if matches:
