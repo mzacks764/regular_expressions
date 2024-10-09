9ba784c9f42f12072eaa5dfe409bc3aaad519073
mzacks764 <mzacks64@outlook.com>
2024-10-08 21:20:27 -0400

    change logic in main to check if site != None before using replace

--- watch.py
+++ watch.py
@@ -4,16 +4,18 @@ def main():
     html = input("HTML: ")
     html = html.strip()
     site = (extract(html))
-    try:
+    if site != None:
         site = site.replace('youtube.','youtu.')
-        print(site)
-    except AttributeError:
-        print('None from main')
+    print(site)
 
 
 
 def extract(html):
-    matches = re.search(r"src=\"(https*://www.youtu\S+)\"", html)
+    # https{0,1} looks for http followed by 0 or 1 's'
+    # \S+ looks for 1 or more non-whitespace characters
+    # \" searches for the quotation marks within html that surround the web address in html
+
+    matches = re.search(r"src=\"(https{0,1}://www.youtu\S+)\"", html)
     if matches:
         print()
         site =matches.groups()
